fedc0fc92a758acf8746f06c9a49fc4f71d96453
tupakako095-ctrl <tupakako095@gmail.com>
2026-03-19 01:59:23 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+const gif = document.querySelector("#gif");
+const text = document.querySelector("#text");
+const vid = document.querySelector("video");
+let count = 2;
+
+const gifs = [
+  "..cat-heart.gif",
+  "..rusure.gif",
+  "..3shocked-1.gif",
+  "..4.crying.gif",
+  "..5.crying.gif",
+  "..idc.gif"
+];
+
+// to load the gifs faster
+gifs.forEach(gifSrc => {
+  const img = new Image();
+  img.src = gifSrc;
+});
+
+// changes for when NO button is clicked
+no.addEventListener("click", () => {
+  if (count == 2) {
+    gif.src = "..rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    text.innerHTML = "Pares tayo??";
+    yes.style.height = "65%";
+    yes.style.width = "60%";
+    no.style.width = "30%";
+    count++;
+  } else if (count == 3) {
+    gif.src = "..3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    text.innerHTML = "Kapag pwede ka?🥹";
+    yes.style.height = "70%";
+    yes.style.width = "70%";
+    no.style.width = "20%";
+    count++;
+  } else if (count == 4) {
+    gif.src = "..4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    text.innerHTML = "Ano tra tra ba?";
+    yes.style.height = "80%";
+    yes.style.width = "80%";
+    no.style.fontSize = "4vh";
+    no.style.width = "10%";
+    count++;
+  } else if (count == 5) {
+    gif.src = "..5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
+    text.innerHTML = "Payag kana?";
+    yes.style.height = "90%";
+    yes.style.width = "96%";
+    no.style.display = "none";
+  }
+});
+
+// changes for when YES button is clicked
+yes.addEventListener("click", () => {
+  vid.style.display = "block";
+  gif.src = "..idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  text.innerHTML = "Yown pumayag karin sheeesh";
+  yes.innerHTML = '<a https://www.facebook.com/carl.jervin.p.malveda">Chat me</a>';// inside the " " put your social media profile link
+  yes.style.height = "90%";
+  yes.style.width = "96%";
+  no.style.display = "none";
+  setTimeout(() => {
+    vid.style.display = "none";
+  }, 9000);
+});
