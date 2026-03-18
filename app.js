357797328cfb764f8425d0425b46e3d059667f6d
tupakako095-ctrl <tupakako095@gmail.com>
2026-03-19 03:15:17 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -23,21 +23,21 @@ gifs.forEach(gifSrc => {
 // changes for when NO button is clicked
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "..rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    gif.src = "rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
     text.innerHTML = "Pares tayo??";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "..3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
     text.innerHTML = "Kapag pwede ka?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "..4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    gif.src = "4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
     text.innerHTML = "Ano tra tra ba?";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +45,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "..5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
+    gif.src = "5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
     text.innerHTML = "Payag kana?";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -56,9 +56,10 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "..idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "idc.gif"; // GIF na lalabas
   text.innerHTML = "Yown pumayag karin sheeesh";
-  yes.innerHTML = '<a https://www.facebook.com/carl.jervin.p.malveda">Chat me</a>';// inside the " " put your social media profile link
+  // Open FB link sa bagong tab
+  window.open("https://www.facebook.com/carl.jervin.p.malveda", "_blank");
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
