390fe2f0506926f82d360246153d7cca129f91a0
tupakako095-ctrl <tupakako095@gmail.com>
2026-03-19 03:33:04 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const vid = document.querySelector("video");
 let count = 2;
 
 const gifs = [
-  "..cat-heart.gif",
-  "..rusure.gif",
-  "..3shocked-1.gif",
-  "..4.crying.gif",
-  "..5.crying.gif",
-  "..idc.gif"
+  "cat-heart.gif",
+  "rusure.gif",
+  "3shocked-1.gif",
+  "4.crying.gif",
+  "5.crying.gif",
+  "idc.gif"
 ];
 
 // to load the gifs faster
@@ -24,21 +24,21 @@ gifs.forEach(gifSrc => {
 no.addEventListener("click", () => {
   if (count == 2) {
     gif.src = "rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
-    text.innerHTML = "Pares tayo??";
+    text.innerHTML = "Pares tayo?";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
     gif.src = "3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
-    text.innerHTML = "Kapag pwede ka?🥹";
+    text.innerHTML = "Pwede kaba?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
     gif.src = "4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
-    text.innerHTML = "Ano tra tra ba?";
+    text.innerHTML = "Ano tra tra?";
     yes.style.height = "80%";
     yes.style.width = "80%";
     no.style.fontSize = "4vh";
@@ -56,10 +56,9 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "idc.gif"; // GIF na lalabas
-  text.innerHTML = "Yown pumayag karin sheeesh";
-  // Open FB link sa bagong tab
-  window.open("https://www.facebook.com/carl.jervin.p.malveda", "_blank");
+  gif.src = "idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  text.innerHTML = "Sawakas, pumayag karin sheesh";
+  yes.innerHTML = '<a href="https://www.facebook.com/carl.jervin.p.malveda">Message me</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
